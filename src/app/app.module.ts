f2e9d160867b3bbd18f573a76cf46e53eff5580b
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-01-24 15:08:05 +0000

    add package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoadingBarHttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
